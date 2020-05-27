61b7bc4a2f94c8cd0c7abd23c3ce0a5517197a8e
pia <fahima.fsp@gmail.com>
2020-05-27 02:28:40 -0500

    Added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
 
+
 @NgModule({
   declarations: [
-    AppComponent
-    
+    AppComponent   
   ],
   imports: [
     BrowserModule,
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,18 +1,25 @@
+import { SharedModule } from './../../shared/shared.module';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
-
-
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { MatSidenavModule} from '@angular/material';
 
 
 @NgModule({
   declarations: [
     DefaultComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostsComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule
+    
   ]
 })
 export class DefaultModule { }
